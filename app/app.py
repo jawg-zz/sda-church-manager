7874555465b3ea55b067cb858f301589a83d69d8
jawg-zz <mosetigeorge@gmail.com>
2026-06-03 22:10:59 +0000

    Fix: load_user graceful when tables don't exist yet

--- app/app.py
+++ app/app.py
@@ -25,8 +25,11 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        from models import User
-        return User.query.get(int(user_id))
+        try:
+            from models import User
+            return User.query.get(int(user_id))
+        except Exception:
+            return None
 
     @app.context_processor
     def inject_church():
